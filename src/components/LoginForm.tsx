090933993e7aac7142633a247c31ae9aa75e5f72
yanisdraa <139394949+yanisdraa@users.noreply.github.com>
2024-11-30 22:11:28 +0100

    Add files via upload

---
+++ src/components/LoginForm.tsx
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from 'react';
+import { Navigate } from 'react-router-dom';
+
+function LoginForm() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [username, setUsername] = useState(() => localStorage.getItem('username') || '');
+  const [password, setPassword] = useState(() => localStorage.getItem('password') || '');
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('username', username);
+  }, [username]);
+
+  useEffect(() => {
+    localStorage.setItem('password', password);
+  }, [password]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    
+    if (username === "admin" && password === "IAZOURENE") {
+      setIsLoggedIn(true);
+    } else {
+      setError('Nom d\'utilisateur ou mot de passe incorrect.');
+    }
+  };
+
+  if (isLoggedIn) {
+    return <Navigate to="/dashboard" />;
+  }
+
+  return (
+    <div className="login-form-container">
+      <form onSubmit={handleSubmit} className="login-form">
+        <h2>Veuillez vous connecter</h2>
+        {error && <p className="error">{error}</p>}
+        <div>
+          <label htmlFor="username">Nom d'utilisateur</label>
+          <input
+            type="text"
+            id="username"
+            placeholder="Entrez votre nom d'utilisateur"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Mot de passe</label>
+          <input
+            type="password"
+            id="password"
+            placeholder="Entrez votre mot de passe"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Se connecter</button>
+      </form>
+    </div>
+  );
+}
+
+export default LoginForm;
